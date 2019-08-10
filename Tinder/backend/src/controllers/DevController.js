3b409952bca9ca9c753e7d5b93d366a5b3dd3390
Leozera <leobhal13@gmail.com>
2019-08-10 12:14:09 -0300

    Finalizando a nossa Api

--- Tinder/backend/src/controllers/DevController.js
+++ Tinder/backend/src/controllers/DevController.js
@@ -2,43 +2,51 @@ const axios = require('axios');
 const Dev = require('../model/Dev');
 
 module.exports = {
-    async index(res, req) {
+    async index(req, res) {
+        try {
+            const { user } = req.headers;
 
-        const { user } = req.headers;
+            const loggedDev = await Dev.findById(user);
 
-        const loggedDev = await Dev.findById(user);
+            const users = await Dev.find({
+                $and: [
+                    { _id: { $ne: user } },
+                    { _id: { $nin: loggedDev.likes } },
+                    { _id: { $nin: loggedDev.deslikes } },
+                ],
+            })
 
-        const users = await Dev.find({
-            $and: [
-                { _id: { $ne: user } },
-                { _id: { $nin: loggedDev.likes } },
-                { _id: { $nin: loggedDev.deslikes } },
-            ],
-        })
+            return res.json(users);
+        } catch (error) {
+            console.log("The response has returned this error" + error);
+        }
 
-        return res.json(users);
     },
 
     async store(req, res) {
+        try {
+            const { username } = req.body;
 
-        const { username } = req.body;
+            const userExists = await Dev.findOne({ user: username });
+            if (userExists) {
+                return res.json(userExists);
+            }
 
-        const userExists = await Dev.findOne({ user: username });
-        if (userExists) {
-            return res.json(userExists);
+            const response = await axios.get(`https://api.github.com/users/${username}`);
+
+            const { name, bio, avatar_url: avatar } = response.data;
+
+            const dev = await Dev.create({
+                name,
+                user: username,
+                bio,
+                avatar
+            })
+
+            return res.json(dev);
+        } catch (error) {
+            console.log("The response has returned this error" + error);
         }
 
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-
-        const { name, bio, avatar_url: avatar } = response.data;
-
-        const dev = await Dev.create({
-            name,
-            user: username,
-            bio,
-            avatar
-        })
-
-        return res.json(dev);
     }
 };
